af041e0fe3aecc997d6a5fd7924762ca8d8f9110
Matteo Latini <matteolatini@nebulab.it>
2016-04-27 12:42:00 +0200

    fixes override path

--- app/overrides/add_retail_store_locators_tab.rb
+++ app/overrides/add_retail_store_locators_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/layouts/admin',
+Deface::Override.new(virtual_path: 'spree/admin/shared/_menu',
                      name: 'add_retail_store_locators_tab',
                      insert_bottom: "[data-hook='admin_tabs']",
                      partial: 'spree/admin/shared/store_locator_menu')
